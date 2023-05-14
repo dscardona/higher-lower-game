036aba93d825043f11d03b6f9bb385c8cf5d5850
Diana Cardona <dscardona@gmail.com>
2023-05-14 10:03:07 -0400

    Create function to generate random accts
    "Create func to select random account"

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 #print logo
+from art import logo
+print(logo)
 #Generate comparison: Compare: A {X}, occupation, nationality)
+from game_data import data
+import random
+
+# Function returns random account
+def random_account():
+    """Returns random instagram account from 'data' array"""
+    return random.choice(data)
+
+# Assign accounts to variables to be compared
+account_a = random_account()
+account_b = random_account()
+
+# Make sure we aren't comparing an account to itself
+while account_a == account_b:
+    account_b = random_account()
+
+print(account_a)
+print(account_b)
+
 #print vs
 #Against B: {Y}, occupation, nationality
 #Ask: WHo has more followers? input 
