db0ffd46f899b0e2ca3bcf89603ce3a7a134e43f
Diana Cardona <dscardona@gmail.com>
2023-05-14 09:39:04 -0400

    Make to do list

---
+++ main.py
@@ -0,0 +1,9 @@
+#print logo
+#Generate comparison: Compare: A {X}, occupation, nationality)
+#print vs
+#Against B: {Y}, occupation, nationality
+#Ask: WHo has more followers? input 
+#Compare followers of A and B, compare input with right answer. 
+# 
+    # If answer is right, print: You're right! current score:. make B (person with less followers) = input and generate new comparison. 
+    #If answer is wrong, end game. Display score and msg.
